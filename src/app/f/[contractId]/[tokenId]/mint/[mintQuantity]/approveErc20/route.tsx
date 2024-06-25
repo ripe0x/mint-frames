fa0e56fc9156d10fde40e1f47607511385857c22
ripe0x <unripeengine@gmail.com>
2024-06-25 19:34:08 -0400

    test approval from new zora sdk params

--- src/app/f/[contractId]/[tokenId]/mint/[mintQuantity]/approveErc20/route.tsx
+++ src/app/f/[contractId]/[tokenId]/mint/[mintQuantity]/approveErc20/route.tsx
@@ -2,12 +2,38 @@ import { Abi, encodeFunctionData, erc20Abi, maxUint256 } from "viem";
 import { frames } from "../../../../frames";
 import { transaction } from "frames.js/core";
 import { CHAIN_ID, ZORA_CONTRACT_ERC20_MINTER_ADDRESS } from "@/constants";
+import { collectorClient, walletClient } from "@/lib/zoraClient";
 
 export const POST = frames(async (ctx) => {
   const tokenContractAddress = ctx.searchParams.erc20TokenAddress;
   const totalPurchaseCost = ctx.searchParams.totalPurchaseCost;
   const spenderAddress = ZORA_CONTRACT_ERC20_MINTER_ADDRESS as `0x${string}`;
   const mintQuantity = ctx.searchParams.mintQuantity;
+  const tokenId = +ctx.url.pathname.split("/")[3];
+  const accountAddress = ctx.message?.connectedAddress as `0x${string}`;
+  const contractAddress = ctx.url.pathname.split("/")[2] as `0x${string}`; // "f/[contractId]"
+
+  const { parameters, erc20Approval } = await collectorClient.mint({
+    tokenContract: contractAddress,
+    mintType: "1155",
+    tokenId: BigInt(tokenId),
+    quantityToMint: 3,
+    mintComment: "My comment",
+    minterAccount: accountAddress,
+  });
+  console.log("parameters", parameters);
+  console.log("erc20Approval", erc20Approval);
+  console.log("totalPurchaseCost", totalPurchaseCost);
+
+  // // request necessary erc20 approvals as returned from the sdk's mint call
+  // const approveHash = await walletClient.writeContract({
+  //   abi: erc20Abi,
+  //   address: erc20Approval!.erc20,
+  //   functionName: "approve",
+  //   args: [erc20Approval!.approveTo, erc20Approval!.quantity],
+  //   account: accountAddress,
+  // });
+  // console.log("approveHash", approveHash);
 
   const calldata = encodeFunctionData({
     abi: erc20Abi,
@@ -16,7 +42,8 @@ export const POST = frames(async (ctx) => {
       spenderAddress as `0x${string}`,
       // BigInt(totalPurchaseCost) * BigInt(mintQuantity),
       // maxUint256,
-      BigInt(totalPurchaseCost),
+      // BigInt(totalPurchaseCost),
+      erc20Approval!.quantity,
     ],
   });
 
--- src/lib/zoraClient.ts
+++ src/lib/zoraClient.ts
@@ -1,8 +1,15 @@
 import { createCollectorClient } from "@zoralabs/protocol-sdk";
 import { publicClient } from "./viemClient";
-import { CHAIN_ID } from "@/constants";
+import { BASE_PROVIDER, CHAIN_ID } from "@/constants";
+import { createWalletClient, http } from "viem";
+import { base } from "viem/chains";
 
 export const collectorClient = createCollectorClient({
   chainId: CHAIN_ID,
   publicClient,
+});
+
+export const walletClient = createWalletClient({
+  chain: base,
+  transport: http(BASE_PROVIDER),
 });
